4320451eef2faddac23fca6e2f26ff4883137b20
planit <planit@tanjeffreyz.github.io>
2023-09-21 16:59:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,150 +1,199 @@
 assignments = {
-  "COMPSCI 164-LEC-001": [
+  "CS62": [
     {
-      "title": "hw0",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-08-31T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3079267/submissions/188466880",
+      "title": "Lab 0 - Introduction to Java and VS Code",
+      "course": "CS62",
+      "dueDate": "2023-09-01T23:59:00",
+      "link": "https://www.gradescope.com/courses/542293/assignments/2901247/submissions/189804355",
       "submitted": true
     },
     {
-      "title": "Drill 1",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-08-31T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3092782/submissions/188472050",
-      "submitted": true
-    },
-    {
-      "title": "hw1",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-09-14T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3207450/submissions/192232186",
-      "submitted": true
-    },
-    {
-      "title": "Drill 2",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-09-07T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3208606/submissions/190301789",
-      "submitted": true
-    },
-    {
-      "title": "Drill 3",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-09-14T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3283256/submissions/192093313",
-      "submitted": true
-    },
-    {
-      "title": "hw2-written",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-09-21T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3327431/submissions/194704702",
-      "submitted": true
-    },
-    {
-      "title": "hw2-code",
-      "course": "COMPSCI 164-LEC-001",
-      "dueDate": "2023-09-21T21:00:00",
-      "link": "https://www.gradescope.com/courses/572216/assignments/3327433/submissions/194758420",
-      "submitted": true
-    }
-  ],
-  "CS 180/280A": [
-    {
-      "title": "Project 1 - Colorizing the Prokudin-Gorskii Photo Collection",
-      "course": "CS 180/280A",
-      "dueDate": "2023-09-06T23:59:00",
-      "link": "https://www.gradescope.com/courses/581661/assignments/3186346/submissions/189914303",
-      "submitted": true
-    },
-    {
-      "title": "Project 2 - Fun with Filters and Frequencies",
-      "course": "CS 180/280A",
-      "dueDate": "2023-09-18T23:59:00",
-      "link": "https://www.gradescope.com/courses/581661/assignments/3186347/submissions/194814806",
-      "submitted": true
-    },
-    {
-      "title": "Project 3 - Face Morphing and Modelling a Photo Collection",
-      "course": "CS 180/280A",
-      "dueDate": "2023-10-02T23:59:00",
-      "link": "https://www.gradescope.com/courses/581661",
-      "submitted": false
-    }
-  ],
-  "CS 189/289A": [
-    {
-      "title": "HW 1 Code",
-      "course": "CS 189/289A",
+      "title": "Lab 1 - The Silver Dollar Game",
+      "course": "CS62",
       "dueDate": "2023-09-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/575262/assignments/3184862/submissions/191544416",
+      "link": "https://www.gradescope.com/courses/542293/assignments/3276962/submissions/191458901",
       "submitted": true
     },
     {
-      "title": "HW 1 Written",
-      "course": "CS 189/289A",
+      "title": "Assignment 1: Graphics Silver Dollar Game",
+      "course": "CS62",
+      "dueDate": "2023-09-14T23:59:00",
+      "link": "https://www.gradescope.com/courses/542293/assignments/3282536/submissions/191974017",
+      "submitted": true
+    },
+    {
+      "title": "Assignment 2 - Week I : Darwin",
+      "course": "CS62",
+      "dueDate": "2023-09-21T23:59:00",
+      "link": "https://www.gradescope.com/courses/542293/assignments/3350794/submissions/195176441",
+      "submitted": true
+    }
+  ],
+  "Math 151": [
+    {
+      "title": "Assignment 1",
+      "course": "Math 151",
       "dueDate": "2023-09-08T23:59:00",
-      "link": "https://www.gradescope.com/courses/575262/assignments/3192847/submissions/191597948",
+      "link": "https://www.gradescope.com/courses/589847/assignments/3184538/submissions/190408074",
       "submitted": true
     },
     {
-      "title": "HW 2 Written",
-      "course": "CS 189/289A",
+      "title": "Assignment 2",
+      "course": "Math 151",
+      "dueDate": "2023-09-15T23:59:00",
+      "link": "https://www.gradescope.com/courses/589847/assignments/3271963/submissions/193455801",
+      "submitted": true
+    },
+    {
+      "title": "Assignment 3",
+      "course": "Math 151",
+      "dueDate": "2023-09-20T23:59:00",
+      "link": "https://www.gradescope.com/courses/589847/assignments/3306885/submissions/195185253",
+      "submitted": true
+    },
+    {
+      "title": "Assignment 4",
+      "course": "Math 151",
       "dueDate": "2023-09-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/575262",
+      "link": "https://www.gradescope.com/courses/589847",
       "submitted": false
     },
     {
-      "title": "HW 2 Code",
-      "course": "CS 189/289A",
-      "dueDate": "2023-09-22T23:59:00",
-      "link": "https://www.gradescope.com/courses/575262",
+      "title": "Assignment 5",
+      "course": "Math 151",
+      "dueDate": "2023-09-25T23:59:00",
+      "link": "https://www.gradescope.com/courses/589847",
       "submitted": false
     }
   ],
-  "CS198-008": [
+  "PO MATH 102.1 FA23": [
     {
-      "title": "Lab 01",
-      "course": "CS198-008",
-      "dueDate": "2023-09-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3255167/submissions/190953989",
+      "title": "Homework Assignment #1",
+      "course": "PO MATH 102.1 FA23",
+      "dueDate": "2023-09-08T10:00:00",
+      "link": "https://www.gradescope.com/courses/590254/assignments/3187131/submissions/191258413",
       "submitted": true
     },
     {
-      "title": "Vitamin 1",
-      "course": "CS198-008",
-      "dueDate": "2023-09-10T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3264956/submissions/190653419",
+      "title": "Homework Assignment #2",
+      "course": "PO MATH 102.1 FA23",
+      "dueDate": "2023-09-15T10:00:00",
+      "link": "https://www.gradescope.com/courses/590254/assignments/3187215/submissions/193267683",
       "submitted": true
     },
     {
-      "title": "Lab 02",
-      "course": "CS198-008",
-      "dueDate": "2023-09-16T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3310726/submissions/192232735",
+      "title": "Homework Assignment #3",
+      "course": "PO MATH 102.1 FA23",
+      "dueDate": "2023-09-22T10:00:00",
+      "link": "https://www.gradescope.com/courses/590254",
+      "submitted": false
+    }
+  ],
+  "PO MATH 131.1 FA23": [
+    {
+      "title": "Exercise (1)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-08T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3255757/submissions/190409016",
       "submitted": true
     },
     {
-      "title": "Vitamin 2",
-      "course": "CS198-008",
-      "dueDate": "2023-09-16T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3316849/submissions/192554276",
+      "title": "Exercise (2)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-08T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3255783/submissions/190409087",
       "submitted": true
     },
     {
-      "title": "Vitamin 3",
-      "course": "CS198-008",
-      "dueDate": "2023-09-23T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3320400/submissions/194823309",
+      "title": "Exercise (3)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-08T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3255803/submissions/191265919",
       "submitted": true
     },
     {
-      "title": "Lab 03",
-      "course": "CS198-008",
-      "dueDate": "2023-09-23T23:59:00",
-      "link": "https://www.gradescope.com/courses/572351/assignments/3321098/submissions/193825423",
+      "title": "Exercise (4)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-08T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3255827/submissions/190409223",
       "submitted": true
+    },
+    {
+      "title": "Exercise (5)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-15T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3267771/submissions/191651879",
+      "submitted": true
+    },
+    {
+      "title": "Exercise (6)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-15T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3267785/submissions/191659322",
+      "submitted": true
+    },
+    {
+      "title": "Exercise (7)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-15T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3328488/submissions/192926099",
+      "submitted": true
+    },
+    {
+      "title": "Exercise (8)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-15T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3328505/submissions/192926502",
+      "submitted": true
+    },
+    {
+      "title": "Exercise (9)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-15T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3328536/submissions/192926562",
+      "submitted": true
+    },
+    {
+      "title": "Exercise (10)",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-15T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140/assignments/3328548/submissions/192926625",
+      "submitted": true
+    },
+    {
+      "title": "11",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-22T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140",
+      "submitted": false
+    },
+    {
+      "title": "12",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-22T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140",
+      "submitted": false
+    },
+    {
+      "title": "13",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-22T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140",
+      "submitted": false
+    },
+    {
+      "title": "15",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-22T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140",
+      "submitted": false
+    },
+    {
+      "title": "18",
+      "course": "PO MATH 131.1 FA23",
+      "dueDate": "2023-09-22T23:55:00",
+      "link": "https://www.gradescope.com/courses/606140",
+      "submitted": false
     }
   ]
 };
