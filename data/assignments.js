5ecea32530cd03c0ed7a96c0ddf22ea9ceda1e26
planit <planit@tanjeffreyz.github.io>
2023-09-28 21:36:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -32,8 +32,8 @@ assignments = {
       "title": "Assignment 2 - Week II : Darwin",
       "course": "CS62",
       "dueDate": "2023-09-28T23:59:00",
-      "link": "https://www.gradescope.com/courses/542293",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/542293/assignments/3393481/submissions/197668964",
+      "submitted": true
     }
   ],
   "Math 151": [
