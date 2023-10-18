eb93d3883b28638910cda97a9f3bdac994e23c2b
planit <planit@tanjeffreyz.github.io>
2023-10-18 20:36:55 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -53,8 +53,8 @@ assignments = {
       "title": "Assignment 4 - Week II : Compression",
       "course": "CS62",
       "dueDate": "2023-10-19T23:59:00",
-      "link": "https://www.gradescope.com/courses/542293",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/542293/assignments/3537129/submissions/203827093",
+      "submitted": true
     }
   ],
   "Math 151": [
