481eec84964af20e2cc75c260822a25ebe8ac090
planit <planit@tanjeffreyz.github.io>
2023-10-19 02:36:58 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -113,6 +113,13 @@ assignments = {
       "dueDate": "2023-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/589847",
       "submitted": false
+    },
+    {
+      "title": "Exam 1",
+      "course": "Math 151",
+      "dueDate": "2023-10-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/589847",
+      "submitted": false
     }
   ],
   "PO MATH 102.1 FA23": [
