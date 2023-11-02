77440930cacf08c4df6b50275670e21a1c9db013
planit <planit@tanjeffreyz.github.io>
2023-11-02 03:36:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -67,7 +67,7 @@ assignments = {
       "title": "Assignment 6 - Autocomplete",
       "course": "CS62",
       "dueDate": "2023-11-02T23:59:00",
-      "link": "https://www.gradescope.com/courses/542293/assignments/3593218/submissions/208365987",
+      "link": "https://www.gradescope.com/courses/542293/assignments/3593218/submissions/208622756",
       "submitted": true
     }
   ],
