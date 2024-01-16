dc4032fdb6a8df5af5644829f9f21117d78fd2f0
planit <planit@tanjeffreyz.github.io>
2024-01-16 03:36:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,12 @@
 assignments = {
+  "CHEM29_S24": [
+    {
+      "title": "PS1, CHEM29, S24",
+      "course": "CHEM29_S24",
+      "dueDate": "2024-01-24T19:00:00",
+      "link": "https://www.gradescope.com/courses/686490",
+      "submitted": false
+    }
+  ],
   "CSCI101": []
 };
