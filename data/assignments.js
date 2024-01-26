401967b83f766ec9ebf4119ac01a2ce64a63c806
planit <planit@tanjeffreyz.github.io>
2024-01-26 02:37:18 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -20,6 +20,13 @@ assignments = {
       "dueDate": "2024-01-31T20:00:00",
       "link": "https://www.gradescope.com/courses/686490",
       "submitted": false
+    },
+    {
+      "title": "Experiment 1: Glassware and Sugar Density",
+      "course": "CHEM29_S24",
+      "dueDate": "2024-02-02T23:59:00",
+      "link": "https://www.gradescope.com/courses/686490/assignments/3997937/submissions/new",
+      "submitted": false
     }
   ],
   "CS 105": [
