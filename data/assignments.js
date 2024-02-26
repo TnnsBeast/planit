c579038611c289ad28ea78055148114cd2ac9e0b
planit <planit@tanjeffreyz.github.io>
2024-02-26 05:36:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -269,7 +269,7 @@ assignments = {
       "title": "week06-ps-written",
       "course": "CSCI101",
       "dueDate": "2024-02-25T22:00:00",
-      "link": "https://www.gradescope.com/courses/702925/assignments/4063582/submissions/233996890",
+      "link": "https://www.gradescope.com/courses/702925/assignments/4063582/submissions/233701397",
       "submitted": true
     },
     {
