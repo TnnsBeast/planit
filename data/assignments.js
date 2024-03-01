6e7cf91c0f04ebe112432571693c1b045c8312fe
planit <planit@tanjeffreyz.github.io>
2024-03-01 22:37:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -83,6 +83,13 @@ assignments = {
       "dueDate": "2024-03-07T20:00:00",
       "link": "https://www.gradescope.com/courses/686490",
       "submitted": false
+    },
+    {
+      "title": "Experiment 6: Calorimetry",
+      "course": "CHEM29_S24",
+      "dueDate": "2024-03-08T23:59:00",
+      "link": "https://www.gradescope.com/courses/686490/assignments/4184386/submissions/new",
+      "submitted": false
     }
   ],
   "CS 105": [
