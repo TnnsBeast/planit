36817d019c302a291fb7a637e0ab97e519a5b8e9
planit <planit@tanjeffreyz.github.io>
2024-03-21 04:36:34 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -141,6 +141,13 @@ assignments = {
       "dueDate": "2024-03-19T23:59:00",
       "link": "https://www.gradescope.com/courses/708537/assignments/4166419/submissions/239734481",
       "submitted": true
+    },
+    {
+      "title": "A6 - dmlab",
+      "course": "CS 105",
+      "dueDate": "2024-03-26T23:59:00",
+      "link": "https://www.gradescope.com/courses/708537",
+      "submitted": false
     }
   ],
   "CS140": [
