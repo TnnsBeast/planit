54d71be1a03f0f314fa7f8c398bb71bb8bb8dbb9
planit <planit@tanjeffreyz.github.io>
2024-03-22 01:31:00 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -97,6 +97,13 @@ assignments = {
       "dueDate": "2024-03-22T23:59:00",
       "link": "https://www.gradescope.com/courses/686490/assignments/4208292/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Experiment 8: Colligative properties",
+      "course": "CHEM29_S24",
+      "dueDate": "2024-03-29T23:59:00",
+      "link": "https://www.gradescope.com/courses/686490/assignments/4253741/submissions/new",
+      "submitted": false
     }
   ],
   "CS 105": [
