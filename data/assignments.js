9fafa4bb47ae715867e3a4c2dd52469e03fff551
planit <planit@tanjeffreyz.github.io>
2024-04-01 04:36:28 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -295,8 +295,8 @@ assignments = {
       "title": "Assignment 8",
       "course": "CS140",
       "dueDate": "2024-03-31T23:59:00",
-      "link": "https://www.gradescope.com/courses/716852",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/716852/assignments/4284092/submissions/243157397",
+      "submitted": true
     }
   ],
   "CSCI101": [
