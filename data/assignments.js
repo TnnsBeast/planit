e04257f1db3de296615e0d8e5ce153e866ad14de
planit <planit@tanjeffreyz.github.io>
2024-04-15 07:36:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -116,7 +116,7 @@ assignments = {
       "title": "Experiment 10: Potentiometric Titration",
       "course": "CHEM29_S24",
       "dueDate": "2024-04-12T23:59:00",
-      "link": "https://www.gradescope.com/courses/686490/assignments/4316908/submissions/new",
+      "link": "https://www.gradescope.com/courses/686490",
       "submitted": false
     }
   ],
