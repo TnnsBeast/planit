da8b63cb5bafb605b66ab295b1dbc38b52ffbd67
planit <planit@tanjeffreyz.github.io>
2024-05-02 07:36:27 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -144,7 +144,7 @@ assignments = {
       "title": "Experiment 13: Chemical Kinetics",
       "course": "CHEM29_S24",
       "dueDate": "2024-05-01T23:59:00",
-      "link": "https://www.gradescope.com/courses/686490/assignments/4393316/submissions/new",
+      "link": "https://www.gradescope.com/courses/686490",
       "submitted": false
     }
   ],
