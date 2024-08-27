16da04e2bf1bc573c496cb04061448cf416539f4
planit <planit@tanjeffreyz.github.io>
2024-08-27 18:41:14 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,12 @@
 assignments = {
-  "CS 131": []
+  "CS 131": [],
+  "MATH113  PO.01": [
+    {
+      "title": "HW01",
+      "course": "MATH113  PO.01",
+      "dueDate": "2024-09-04T23:59:00",
+      "link": "https://www.gradescope.com/courses/814802",
+      "submitted": false
+    }
+  ]
 };
