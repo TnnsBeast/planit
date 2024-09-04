6d2d698178ab167782f4bfcc26fd860432ce6839
planit <planit@tanjeffreyz.github.io>
2024-09-04 07:37:44 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -52,6 +52,13 @@ assignments = {
       "dueDate": "2024-09-11T23:59:00",
       "link": "https://www.gradescope.com/courses/814802",
       "submitted": false
+    },
+    {
+      "title": "HW03",
+      "course": "MATH113  PO.01",
+      "dueDate": "2024-09-18T23:59:00",
+      "link": "https://www.gradescope.com/courses/814802",
+      "submitted": false
     }
   ]
 };
