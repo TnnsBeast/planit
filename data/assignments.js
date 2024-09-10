4763a9323494ff72b5704ce9757f7ac30cff0d4b
planit <planit@tanjeffreyz.github.io>
2024-09-10 05:39:03 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -11,8 +11,8 @@ assignments = {
       "title": "Module 2 Reflection",
       "course": "CS 131",
       "dueDate": "2024-09-09T23:59:00",
-      "link": "https://www.gradescope.com/courses/777935/assignments/4450674/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/777935/assignments/4450674/submissions/265819601",
+      "submitted": true
     },
     {
       "title": "HW 1: peoPLe",
