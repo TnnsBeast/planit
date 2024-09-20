fe3953cd6503b81216167492b283413d3d7341f1
planit <planit@tanjeffreyz.github.io>
2024-09-20 22:40:38 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -18,8 +18,8 @@ assignments = {
       "title": "HW 4: Your favorite image",
       "course": "CS 131",
       "dueDate": "2024-09-23T23:59:00",
-      "link": "https://www.gradescope.com/courses/777935/assignments/4450673/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/777935/assignments/4450673/submissions/269771326",
+      "submitted": true
     },
     {
       "title": "Module 2 Reflection",
