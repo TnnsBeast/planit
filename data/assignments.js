07596e3822efdd6d086e0e644076640180fec9de
planit <planit@tanjeffreyz.github.io>
2024-09-21 16:42:39 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -83,6 +83,13 @@ assignments = {
       "dueDate": "2024-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4927564/submissions/268306445",
       "submitted": true
+    },
+    {
+      "title": "Module 4 Reflection",
+      "course": "CS 131",
+      "dueDate": "2024-09-23T23:59:00",
+      "link": "https://www.gradescope.com/courses/777935/assignments/4960805/submissions/new",
+      "submitted": false
     }
   ],
   "ECON101  PO.01": [
