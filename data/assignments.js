4194c40156eece2c0a4242a49a4d7f70a1a2c0c1
planit <planit@tanjeffreyz.github.io>
2024-10-12 15:37:26 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -123,8 +123,8 @@ assignments = {
       "title": "Module 6 Reflection",
       "course": "CS 131",
       "dueDate": "2024-10-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/777935/assignments/5120314/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/777935/assignments/5120314/submissions/277293931",
+      "submitted": true
     },
     {
       "title": "Module 6 Lab: Parsing",
