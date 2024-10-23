aa898c661bc448a15c416945e60a5d1c2461c9a7
planit <planit@tanjeffreyz.github.io>
2024-10-23 05:40:30 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -158,8 +158,8 @@ assignments = {
       "title": "Quiz 2",
       "course": "CS 131",
       "dueDate": "2024-10-24T23:59:00",
-      "link": "https://www.gradescope.com/courses/777935/assignments/5137465/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/777935/assignments/5137465/submissions/280978108",
+      "submitted": true
     },
     {
       "title": "Module 7.2 Monad Lab",
