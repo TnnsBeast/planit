6ba9516afa1123f4e89409def07b7525de6b89ac
planit <planit@tanjeffreyz.github.io>
2024-11-12 18:43:29 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -242,8 +242,8 @@ assignments = {
       "title": "Quiz 3",
       "course": "CS 131",
       "dueDate": "2024-11-21T23:59:00",
-      "link": "https://www.gradescope.com/courses/777935/assignments/5289364/submissions/new",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/777935/assignments/5289364/submissions/288167235",
+      "submitted": true
     }
   ],
   "ECON101  PO.01": [
