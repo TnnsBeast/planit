b3c9bab37a62bf6e67f18719d2267a1dac1233be
planit <planit@tanjeffreyz.github.io>
2025-01-01 02:11:08 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -3,336 +3,336 @@ assignments = {
     {
       "title": "Lab: ghci",
       "course": "CS 131",
-      "dueDate": "2024-09-03T14:30:00",
+      "dueDate": "2025-09-03T14:30:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450662/submissions/263994588",
       "submitted": true
     },
     {
       "title": "Quiz 1 Retake",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 4: Little Languages (Random Art and Regular Expressions)",
       "course": "CS 131",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 4: Your favorite image",
       "course": "CS 131",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450673/submissions/269771326",
       "submitted": true
     },
     {
       "title": "Module 2 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-09-09T23:59:00",
+      "dueDate": "2025-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450674/submissions/265819601",
       "submitted": true
     },
     {
       "title": "HW 1: peoPLe",
       "course": "CS 131",
-      "dueDate": "2024-09-03T23:59:00",
+      "dueDate": "2025-09-03T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450677/submissions/262914614",
       "submitted": true
     },
     {
       "title": "HW 2: FUNctional programming",
       "course": "CS 131",
-      "dueDate": "2024-09-09T23:59:00",
+      "dueDate": "2025-09-09T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450684/submissions/265090004",
       "submitted": true
     },
     {
       "title": "Module 5 Lab: pic and PS",
       "course": "CS 131",
-      "dueDate": "2024-09-24T23:59:00",
+      "dueDate": "2025-09-24T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450688/submissions/271093428",
       "submitted": true
     },
     {
       "title": "Quiz 1",
       "course": "CS 131",
-      "dueDate": "2024-09-19T23:59:00",
+      "dueDate": "2025-09-19T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450691/submissions/269141466",
       "submitted": true
     },
     {
       "title": "Module 3 Lab: Lists / Data Types",
       "course": "CS 131",
-      "dueDate": "2024-09-10T23:59:00",
+      "dueDate": "2025-09-10T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4450723/submissions/265940121",
       "submitted": true
     },
     {
       "title": "Module 4 Lab: Implementing a Boolean language",
       "course": "CS 131",
-      "dueDate": "2024-09-17T23:59:00",
+      "dueDate": "2025-09-17T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 3: Lists, Datatypes, and a Stack Machine",
       "course": "CS 131",
-      "dueDate": "2024-09-16T23:59:00",
+      "dueDate": "2025-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 6: Compiling Pic to PS (pic2ps) Part 2",
       "course": "CS 131",
-      "dueDate": "2024-10-07T23:59:00",
+      "dueDate": "2025-10-07T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 5: pic2ps Part 1",
       "course": "CS 131",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Module 3 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-09-16T23:59:00",
+      "dueDate": "2025-09-16T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4927564/submissions/268306445",
       "submitted": true
     },
     {
       "title": "Module 4 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-09-23T23:59:00",
+      "dueDate": "2025-09-23T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/4960805/submissions/270797725",
       "submitted": true
     },
     {
       "title": "Module 5 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-09-30T23:59:00",
+      "dueDate": "2025-09-30T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5054453/submissions/272841988",
       "submitted": true
     },
     {
       "title": "Module 6 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-10-11T23:59:00",
+      "dueDate": "2025-10-11T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5120314/submissions/277293931",
       "submitted": true
     },
     {
       "title": "Module 6 Lab: Parsing",
       "course": "CS 131",
-      "dueDate": "2024-10-08T23:59:00",
+      "dueDate": "2025-10-08T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 7: Compiling Pic to PS (pic2ps) Part 3",
       "course": "CS 131",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 7: Your pic Program and Picture",
       "course": "CS 131",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Module 7 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-10-21T23:59:00",
+      "dueDate": "2025-10-21T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5124828/submissions/280361372",
       "submitted": true
     },
     {
       "title": "Quiz 2",
       "course": "CS 131",
-      "dueDate": "2024-10-24T23:59:00",
+      "dueDate": "2025-10-24T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5137465/submissions/280978108",
       "submitted": true
     },
     {
       "title": "Module 7 Lab: Functors and Monads",
       "course": "CS 131",
-      "dueDate": "2024-10-17T23:59:00",
+      "dueDate": "2025-10-17T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Module 8 Lab: Moses",
       "course": "CS 131",
-      "dueDate": "2024-10-22T23:59:00",
+      "dueDate": "2025-10-22T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 8: Moses (Typechecking)",
       "course": "CS 131",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 8: Your Moses Programs",
       "course": "CS 131",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Module 9 Lab: Lambda Calculus",
       "course": "CS 131",
-      "dueDate": "2024-10-29T23:59:00",
+      "dueDate": "2025-10-29T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Module 8 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-10-28T23:59:00",
+      "dueDate": "2025-10-28T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5216540/submissions/282393306",
       "submitted": true
     },
     {
       "title": "HW 9: Lambda Calculus (lc)",
       "course": "CS 131",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Module 9 Reflection",
       "course": "CS 131",
-      "dueDate": "2024-11-04T23:59:00",
+      "dueDate": "2025-11-04T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5258434/submissions/284951107",
       "submitted": true
     },
     {
       "title": "Module 10.1 Completion",
       "course": "CS 131",
-      "dueDate": "2024-11-07T23:59:00",
+      "dueDate": "2025-11-07T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "HW 10: Critiquing PLs",
       "course": "CS 131",
-      "dueDate": "2024-11-11T23:59:00",
+      "dueDate": "2025-11-11T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Deliverable: Project Choice",
       "course": "CS 131",
-      "dueDate": "2024-11-12T23:59:00",
+      "dueDate": "2025-11-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5283254/submissions/286965177",
       "submitted": true
     },
     {
       "title": "Quiz 3",
       "course": "CS 131",
-      "dueDate": "2024-11-21T23:59:00",
+      "dueDate": "2025-11-21T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5289364/submissions/288167235",
       "submitted": true
     },
     {
       "title": "Quiz 2 Retake",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Project Initial Questions",
       "course": "CS 131",
-      "dueDate": "2024-11-18T23:59:00",
+      "dueDate": "2025-11-18T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5343849/submissions/290531092",
       "submitted": true
     },
     {
       "title": "Quiz 3 Retake",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Project [Critiquing PLs] -- Artifacts",
       "course": "CS 131",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Project [Lambda Expressions] -- Artifacts",
       "course": "CS 131",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Project [Raskell] -- Artifacts",
       "course": "CS 131",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Project [Turing Machines] -- Artifacts",
       "course": "CS 131",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Project [Program Analysis] -- Artifacts",
       "course": "CS 131",
-      "dueDate": "2024-12-06T23:59:00",
+      "dueDate": "2025-12-06T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5411326/submissions/294978167",
       "submitted": true
     },
     {
       "title": "Critiquing PLs reflection",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Lambda Expressions Reflection",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Raskell Reflection",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Turing Machine Reflection",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935",
       "submitted": false
     },
     {
       "title": "Program Analysis Reflection",
       "course": "CS 131",
-      "dueDate": "2024-12-12T23:59:00",
+      "dueDate": "2025-12-12T23:59:00",
       "link": "https://www.gradescope.com/courses/777935/assignments/5429796/submissions/295983646",
       "submitted": true
     }
@@ -341,28 +341,28 @@ assignments = {
     {
       "title": "Essay Assignment",
       "course": "ECON101  PO.01",
-      "dueDate": "2024-09-15T23:59:00",
+      "dueDate": "2025-09-15T23:59:00",
       "link": "https://www.gradescope.com/courses/847747/assignments/4847474/submissions/267562424",
       "submitted": true
     },
     {
       "title": "R lab 1",
       "course": "ECON101  PO.01",
-      "dueDate": "2024-10-06T23:59:00",
+      "dueDate": "2025-10-06T23:59:00",
       "link": "https://www.gradescope.com/courses/847747/assignments/4970995/submissions/271112586",
       "submitted": true
     },
     {
       "title": "R lab 2",
       "course": "ECON101  PO.01",
-      "dueDate": "2024-10-27T23:59:00",
+      "dueDate": "2025-10-27T23:59:00",
       "link": "https://www.gradescope.com/courses/847747/assignments/5149563/submissions/282271640",
       "submitted": true
     },
     {
       "title": "R Lab 3",
       "course": "ECON101  PO.01",
-      "dueDate": "2024-11-24T23:59:00",
+      "dueDate": "2025-11-24T23:59:00",
       "link": "https://www.gradescope.com/courses/847747/assignments/5240760/submissions/292142871",
       "submitted": true
     }
@@ -371,105 +371,105 @@ assignments = {
     {
       "title": "HW01",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-09-04T23:59:00",
+      "dueDate": "2025-09-04T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688480/submissions/263723345",
       "submitted": true
     },
     {
       "title": "HW02",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-09-11T23:59:00",
+      "dueDate": "2025-09-11T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688493/submissions/266577815",
       "submitted": true
     },
     {
       "title": "HW03",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-09-18T23:59:00",
+      "dueDate": "2025-09-18T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688683/submissions/268987464",
       "submitted": true
     },
     {
       "title": "HW04",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-09-25T23:59:00",
+      "dueDate": "2025-09-25T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688686/submissions/271562271",
       "submitted": true
     },
     {
       "title": "HW05",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-10-02T23:59:00",
+      "dueDate": "2025-10-02T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688700/submissions/273967037",
       "submitted": true
     },
     {
       "title": "HW06",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-10-11T23:59:00",
+      "dueDate": "2025-10-11T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688701/submissions/277255729",
       "submitted": true
     },
     {
       "title": "HW07",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-10-18T23:59:00",
+      "dueDate": "2025-10-18T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688702/submissions/279432665",
       "submitted": true
     },
     {
       "title": "HW08",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-10-23T23:59:00",
+      "dueDate": "2025-10-23T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688703/submissions/284025594",
       "submitted": true
     },
     {
       "title": "HW09",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-10-30T23:59:00",
+      "dueDate": "2025-10-30T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688704/submissions/283944570",
       "submitted": true
     },
     {
       "title": "HW10",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-11-08T23:59:00",
+      "dueDate": "2025-11-08T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4688716/submissions/286757817",
       "submitted": true
     },
     {
       "title": "HW11",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-11-13T23:59:00",
+      "dueDate": "2025-11-13T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4689281/submissions/287068845",
       "submitted": true
     },
     {
       "title": "HW12",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-11-20T23:59:00",
+      "dueDate": "2025-11-20T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4689286/submissions/290106572",
       "submitted": true
     },
     {
       "title": "HW13",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-11-27T23:59:00",
+      "dueDate": "2025-11-27T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4689298/submissions/291362235",
       "submitted": true
     },
     {
       "title": "HW14",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-12-04T23:59:00",
+      "dueDate": "2025-12-04T23:59:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/4689301/submissions/294561807",
       "submitted": true
     },
     {
       "title": "Final examination",
       "course": "MATH113  PO.01",
-      "dueDate": "2024-12-13T17:00:00",
+      "dueDate": "2025-12-13T17:00:00",
       "link": "https://www.gradescope.com/courses/814802/assignments/5335880/submissions/290667616",
       "submitted": true
     }
