3b534bcd79f0d58b2aab59729e8b188a8c61d952
planit <planit@tanjeffreyz.github.io>
2025-01-15 01:56:10 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -1,3 +1,11 @@
 assignments = {
-  "ENGR 154": []
+  "ENGR 154": [
+    {
+      "title": "PS-Ch2",
+      "course": "ENGR 154",
+      "dueDate": "2025-02-03T13:15:00",
+      "link": "https://www.gradescope.com/courses/953449/assignments/5601826/submissions/new",
+      "submitted": false
+    }
+  ]
 };
