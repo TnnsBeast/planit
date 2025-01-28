d3e99c159e4580a43f1d6e2922549e5a3e3ad902
planit <planit@tanjeffreyz.github.io>
2025-01-28 17:38:42 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -24,6 +24,13 @@ assignments = {
       "dueDate": "2025-02-03T13:15:00",
       "link": "https://www.gradescope.com/courses/953449/assignments/5601826/submissions/new",
       "submitted": false
+    },
+    {
+      "title": "Lab 0",
+      "course": "ENGR 154",
+      "dueDate": "2025-01-30T13:15:00",
+      "link": "https://www.gradescope.com/courses/953449/assignments/5685513/submissions/new",
+      "submitted": false
     }
   ],
   "MATH171  PO.01": [
