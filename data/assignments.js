9fb58b04178af11fe3121aa76cc644e74a045287
planit <planit@tanjeffreyz.github.io>
2025-04-12 05:35:49 +0000

    Updated assignments

--- data/assignments.js
+++ data/assignments.js
@@ -388,8 +388,8 @@ assignments = {
       "title": "Homework 08",
       "course": "MATH171  PO.01",
       "dueDate": "2025-04-11T23:59:00",
-      "link": "https://www.gradescope.com/courses/967914",
-      "submitted": false
+      "link": "https://www.gradescope.com/courses/967914/assignments/6005842/submissions/324776975",
+      "submitted": true
     },
     {
       "title": "Homework 09",
